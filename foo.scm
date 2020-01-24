d1d6ebf53bd13f9f3ee12d8c6f568ce4c0867960
Tamar <tamarros94@gmail.com>
2020-01-24 00:48:15 -0800

    ready for submission (:

--- foo.scm
+++ foo.scm
@@ -1,14 +1,1 @@
-(define l4 
-    (list    
-        #{db1}=(1 . #{db4}) 
-        #{db5}=(5 . #{db1})
-        #{db3}=(3 . #{db5})
-        #{db6}=(6 . #{db3})
-        #{db9}=(9 . #{db6})
-        #{db4}=(4 . #{db9})
-))
-
-(equal?
-    (car (cdr (cdr (cdr (cdr (cdr (cdr (car l4))))))))
-    (car (car l4))
-)
+(cons* '1 '2 '())
